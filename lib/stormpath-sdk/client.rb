8073b84172322d2308f830a54d14fa689b7d99d1
Boram Yoon <boram@carbonfive.com>
2013-05-09 14:57:22 -0700

    Remove application instantiation from Client constructor

--- lib/stormpath-sdk/client.rb
+++ lib/stormpath-sdk/client.rb
@@ -23,30 +23,19 @@ module Stormpath
     attr_reader :data_store, :application
 
     def initialize(options)
-      application_url = options[:application_url]
       api_key = options[:api_key]
-      application_path = nil
       base_url = options[:base_url]
 
-      if application_url
-        uri = URI(options[:application_url])
-        if uri.userinfo
-          api_key_id, api_key_secret = uri.userinfo.split(":")
-          api_key = ApiKey.new api_key_id, api_key_secret
-        end
-        application_path = uri.path.slice(/\/applications(.)*$/)
-      end
-
       api_key = if api_key
-                  case api_key
-                  when ApiKey then api_key
-                  when Hash then ApiKey.new api_key[:id], api_key[:secret]
-                  end
-                elsif options[:api_key_file_location]
-                  load_api_key_file options[:api_key_file_location],
-                    options[:api_key_id_property_name],
-                    options[:api_key_secret_property_name]
-                end
+        case api_key
+        when ApiKey then api_key
+        when Hash then ApiKey.new api_key[:id], api_key[:secret]
+        end
+      elsif options[:api_key_file_location]
+        load_api_key_file options[:api_key_file_location],
+          options[:api_key_id_property_name],
+          options[:api_key_secret_property_name]
+      end
 
       assert_not_nil api_key, "No API key has been provided.  Please " +
           "pass an 'api_key' or 'api_key_file_location' to the " +
@@ -54,10 +43,6 @@ module Stormpath
 
       request_executor = Stormpath::Http::HttpClientRequestExecutor.new(api_key)
       @data_store = Stormpath::DataStore.new(request_executor, self, base_url)
-
-      if application_path
-        @application = self.applications.get application_path
-      end
     end
 
     def tenant
