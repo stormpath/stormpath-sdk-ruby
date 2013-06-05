fab63a4e9be189be434685e5c4aeb2ff255dce0f
Boram Yoon <boram@carbonfive.com>
2013-06-05 11:46:13 -0700

    Add cache entry specs

--- spec/cache/cache_entry_spec.rb
+++ spec/cache/cache_entry_spec.rb
@@ -90,4 +90,68 @@ describe Stormpath::Cache::CacheEntry do
       end
     end
   end
+
+  describe '#to_h' do
+    let(:cache_entry) do
+      Stormpath::Cache::CacheEntry.new 'foo'
+    end
+
+    let(:now) { Time.now }
+    before { Timecop.freeze now }
+
+    it 'returns a hash of the attributes' do
+      expect(cache_entry.to_h).to eq({
+        'value' => cache_entry.value,
+        'created_at' => cache_entry.created_at,
+        'last_accessed_at' => cache_entry.last_accessed_at
+      })
+    end
+  end
+
+  describe '.from_h=' do
+    let(:hash) do
+      {
+        "value" =>
+          {
+            "href" => "https://api.stormpath.com/v1/applications/app1",
+            "name" => "application app1",
+            "tenant" => {
+              "href" => "https://api.stormpath.com/v1/tenants/ten2"
+            },
+            "groups" =>
+              {"href" => "https://api.stormpath.com/v1/applications/app3/groups",
+               "items" =>
+                 [{"href" => "https://api.stormpath.com/v1/groups/gro4"},
+                 {"href" => "https://api.stormpath.com/v1/groups/gro5"}]
+              }
+            },
+        "created_at" => "2013-06-05T10:01:31-07:00",
+        "last_accessed_at" => "2013-06-05T10:01:31-07:00"
+      }
+    end
+
+    let(:cache_entry) do
+      Stormpath::Cache::CacheEntry.from_h(hash)
+    end
+
+    let(:created_at) do
+      Time.parse(hash['created_at'])
+    end
+
+    let(:last_accessed_at) do
+      Time.parse(hash['last_accessed_at'])
+    end
+
+    it 'returns a cache entry' do
+      expect(cache_entry).to be_kind_of Stormpath::Cache::CacheEntry
+    end
+
+    it 'sets created at' do
+      expect(cache_entry.created_at).to eq created_at
+    end
+
+    it 'sets the last accessed at' do
+      expect(cache_entry.last_accessed_at).to eq last_accessed_at
+    end
+  end
 end
