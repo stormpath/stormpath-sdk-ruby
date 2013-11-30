4f8b71d93a42d6b61bc8cf8a9f6811eea9f472b2
DamirSvrtan <damir.svrtan@gmail.com>
2013-11-30 13:50:44 +0100

    alias_methods for default_store setters

--- lib/stormpath-sdk/resource/account_store_mapping.rb
+++ lib/stormpath-sdk/resource/account_store_mapping.rb
@@ -5,14 +5,6 @@ class Stormpath::Resource::AccountStoreMapping < Stormpath::Resource::Instance
 
   belongs_to :application
 
-  def account_store_href
-    get_property("accountStore")["href"]
-  end
-
-  def is_directory?(directory_or_group_href)
-    /directories/.match directory_or_group_href
-  end
-
   def account_store
     directory_or_group_href = account_store_href
     if is_directory? directory_or_group_href
@@ -26,6 +18,18 @@ class Stormpath::Resource::AccountStoreMapping < Stormpath::Resource::Instance
   alias_method :default_account_store?, :is_default_account_store
   alias_method :default_group_store, :is_default_group_store
   alias_method :default_group_store?, :is_default_group_store
+  alias_method :default_account_store=, :is_default_account_store=
+  alias_method :default_group_store=, :is_default_group_store=
+
+  private
+
+    def account_store_href
+      get_property("accountStore")["href"]
+    end
+    
+    def is_directory?(directory_or_group_href)
+      /directories/.match directory_or_group_href
+    end
 
 end
 
