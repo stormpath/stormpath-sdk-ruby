acbe31da8aa854b276e76ec5c665820158b0b801
DamirSvrtan <damir.svrtan@gmail.com>
2013-12-18 14:44:27 +0100

    Copyrights added on 3 resources

--- lib/stormpath-sdk/resource/status.rb
+++ lib/stormpath-sdk/resource/status.rb
@@ -1,3 +1,18 @@
+#
+# Copyright 2013 Stormpath, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 module Stormpath::Resource::Status
 
   ENABLED = 'ENABLED'
@@ -12,9 +27,7 @@ module Stormpath::Resource::Status
   def status
     value = get_property STATUS
 
-    if !value.nil?
-      value = value.upcase
-    end
+    value.upcase! if value
 
     value
   end
