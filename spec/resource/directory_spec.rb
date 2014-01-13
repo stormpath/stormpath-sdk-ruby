cd0f260563d241b5012be0020f0628b704d9a6e4
DamirSvrtan <damir.svrtan@gmail.com>
2014-01-13 10:40:50 +0100

    Detailed directory spec for dependent destroy

--- spec/resource/directory_spec.rb
+++ spec/resource/directory_spec.rb
@@ -27,4 +27,49 @@ describe Stormpath::Resource::Directory, :vcr do
       end
     end
   end
+
+
+  describe '#delete_directory' do
+
+    let(:directory) { test_api_client.directories.create name: 'test_directory' }
+
+    let(:application) { test_api_client.applications.create name: 'test_application' }
+
+    let(:reloaded_directory) { test_api_client.directories.get directory.href }
+
+    let(:reloaded_application) { test_api_client.applications.get application.href }
+
+    let(:reloaded_application_2) { test_api_client.applications.get application.href }
+
+    let!(:group) { directory.groups.create name: 'someGroup' }
+
+    let!(:account) { directory.accounts.create({ email: 'rubysdk@example.com', given_name: 'Ruby SDK', password: 'P@$$w0rd',surname: 'SDK' }) }
+
+    let!(:account_store_mapping) do
+      test_api_client.account_store_mappings.create({ application: application, account_store: directory })
+    end
+
+    after do
+      application.delete if application
+    end
+
+    it 'and all of its associations' do
+      expect(reloaded_directory.groups).to have(1).item
+      expect(reloaded_directory.accounts).to have(1).item
+
+      expect(reloaded_application.account_store_mappings.first.account_store).to eq(directory)
+
+      expect(reloaded_application.accounts).to include(account)
+      expect(reloaded_application.groups).to include(group)
+
+      directory.delete
+
+      expect(reloaded_application_2.accounts).not_to include(account)
+      expect(reloaded_application_2.groups).not_to include(group)
+
+      expect(reloaded_application_2.account_store_mappings).to have(0).item
+    end
+
+  end
+
 end
