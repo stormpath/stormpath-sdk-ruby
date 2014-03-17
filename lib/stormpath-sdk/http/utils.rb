3d8df4815e25642754e31d3153a783246ceda4ad
damir <damir.svrtan@gmail.com>
2014-03-17 03:51:59 +0100

    Updating the encode_url HTTP::Util method to use CGI for query strings

--- lib/stormpath-sdk/http/utils.rb
+++ lib/stormpath-sdk/http/utils.rb
@@ -24,26 +24,39 @@ module Stormpath
       end
 
       def encode_url(value, path, canonical)
-        URI.escape(value.to_s).tap do |encoded|
-          if canonical
-            str_map = {'+' => '%20', '*' => '%2A', '%7E' => '~'}
+        if path
+          return URI.escape(value.to_s)
+        else
+          CGI.escape(value.to_s).tap do |encoded|
+            str_map = {'+' => '%20', '%7E' => '~' }
 
             str_map.each do |key, str_value|
               if encoded.include? key
-                encoded[key] = str_value
+                encoded.gsub!(key, str_value)
               end
             end
 
-            # encoded['%7E'] = '~'  --> yes, this is reversed (compared to the other two) intentionally
-
-            if path
-              str = '%2F'
-              if encoded.include? str
-                encoded[str] = '/'
-              end
-            end
           end
         end
+        # URI.escape(value.to_s).tap do |encoded|
+        #   if canonical
+        #     str_map = {'+' => '%20', '*' => '%2A', '@' => '%40', '%7E' => '~' }
+        #     # encoded['%7E'] = '~'  --> yes, this is reversed (compared to the other three) intentionally
+
+        #     str_map.each do |key, str_value|
+        #       if encoded.include? key
+        #         encoded[key] = str_value
+        #       end
+        #     end
+
+        #     if path
+        #       str = '%2F'
+        #       if encoded.include? str
+        #         encoded[str] = '/'
+        #       end
+        #     end
+        #   end
+        # end
       end
 
     end
