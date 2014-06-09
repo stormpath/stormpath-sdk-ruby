622cd96b199c4ed12249fb5f5761ffb9e77f895a
Damir Svrtan <damir.svrtan@gmail.com>
2014-06-09 21:29:53 +0200

    Autoload everything.

--- lib/stormpath-sdk.rb
+++ lib/stormpath-sdk.rb
@@ -58,15 +58,20 @@ module Stormpath
   end
 
   module Authentication
+    autoload :UsernamePasswordRequest, "stormpath-sdk/auth/username_password_request"
+    autoload :BasicLoginAttempt, "stormpath-sdk/auth/basic_login_attempt"
+    autoload :AuthenticationResult, "stormpath-sdk/auth/authentication_result"
+    autoload :BasicAuthenticator, "stormpath-sdk/auth/basic_authenticator"
+  end
+
+  module Http
+    autoload :Utils, "stormpath-sdk/http/utils"
+    autoload :Request, "stormpath-sdk/http/request"
+    autoload :Response, "stormpath-sdk/http/response"
+    autoload :HttpClientRequestExecutor, "stormpath-sdk/http/http_client_request_executor"
+
+    module Authc
+      autoload :Sauthc1Signer, "stormpath-sdk/http/authc/sauthc1_signer"
+    end
   end
 end
-
-require "stormpath-sdk/auth/username_password_request"
-require 'stormpath-sdk/http/utils'
-require "stormpath-sdk/http/request"
-require "stormpath-sdk/http/response"
-require "stormpath-sdk/http/authc/sauthc1_signer"
-require "stormpath-sdk/http/http_client_request_executor"
-require "stormpath-sdk/auth/basic_login_attempt"
-require "stormpath-sdk/auth/authentication_result"
-require "stormpath-sdk/auth/basic_authenticator"
