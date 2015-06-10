30a77b8e53462f0cd7065d9ad15405d688905941
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-06-10 14:06:48 +0200

    add size property to CollectionPage

--- lib/stormpath-sdk/resource/collection.rb
+++ lib/stormpath-sdk/resource/collection.rb
@@ -90,7 +90,7 @@ class Stormpath::Resource::Collection
     class CollectionPage < Stormpath::Resource::Base
       ITEMS = 'items'
 
-      prop_accessor :offset, :limit
+      prop_accessor :offset, :limit, :size
 
       attr_accessor :item_type
 
