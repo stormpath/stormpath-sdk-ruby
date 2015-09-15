cbc973874ec74fad443375133de648ef7d85eef0
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-09-15 16:16:40 +0200

    adding verification_emails call to application

--- lib/stormpath-sdk/resource/application.rb
+++ lib/stormpath-sdk/resource/application.rb
@@ -28,6 +28,7 @@ class Stormpath::Resource::Application < Stormpath::Resource::Instance
   has_many :password_reset_tokens, can: [:get, :create]
   has_many :account_store_mappings, can: [:get, :create]
   has_many :groups, can: [:get, :create]
+  has_many :verification_emails, can: :create
 
   has_one :default_account_store_mapping, class_name: :accountStoreMapping
   has_one :default_group_store_mapping, class_name: :accountStoreMapping
