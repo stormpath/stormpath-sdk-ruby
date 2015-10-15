3050a153e6883751ac564c01d09ce1fe67101511
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-10-15 22:55:44 +0200

    authenticatiro class adding data store dependancy and authenticate method

--- lib/stormpath-sdk/oauth/authenticator.rb
+++ lib/stormpath-sdk/oauth/authenticator.rb
@@ -1,6 +1,19 @@
 module Stormpath
   module Oauth
     class Authenticator
+      def initialize(data_store)
+        @data_store = data_store
+      end
+
+      def authenticate parent_href, options
+        #TODO maybe add here some validations
+
+        attempt = @data_store.instantiate PasswordGrant
+        attempt.set_options(options)
+        
+        href = parent_href + '/oauth/token'
+        @data_store.create href, attempt, Stormpath::Oauth::AccessTokenResponse
+      end
     end
   end
 end
