ad6f3ed7107412a0bef735758bd08b41dd367bbe
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-10-20 23:15:46 +0200

    changes in password grand removing form_data as property and adding it as method

--- lib/stormpath-sdk/oauth/password_grant.rb
+++ lib/stormpath-sdk/oauth/password_grant.rb
@@ -3,14 +3,14 @@ module Stormpath
     class PasswordGrant < Stormpath::Resource::Base
       prop_accessor :grant_type, :username, :password, :form_data
 
-      def initialize
-        set_property :form_data, true
-      end
-
       def set_options(options)
         set_property :grant_type, options[:grant_type]
         set_property :username, options[:username]
         set_property :password, options[:password]
+      end
+
+      def form_data?
+        true
       end
     end
   end
