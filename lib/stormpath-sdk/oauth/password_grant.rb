863203abe418a0d7ed3c4bcae409093f58dd1280
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-10-21 11:04:33 +0200

    changes in password grant object parsing by body and changes in request executor

--- lib/stormpath-sdk/oauth/password_grant.rb
+++ lib/stormpath-sdk/oauth/password_grant.rb
@@ -4,9 +4,9 @@ module Stormpath
       prop_accessor :grant_type, :username, :password, :form_data
 
       def set_options(options)
-        set_property :grant_type, options[:grant_type]
-        set_property :username, options[:username]
-        set_property :password, options[:password]
+        set_property :grant_type, options[:body][:grant_type]
+        set_property :username, options[:body][:username]
+        set_property :password, options[:body][:password]
       end
 
       def form_data?
