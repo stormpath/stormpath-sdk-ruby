c2c54f7aa4a806cdbbf280d1f420c0dd7128cf7e
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-11-03 12:58:58 +0100

    adding tests for organization account store mappings

--- spec/resource/organization_spec.rb
+++ spec/resource/organization_spec.rb
@@ -57,4 +57,60 @@ describe Stormpath::Resource::Organization, :vcr do
       expect(org.name_key).to eq("changed-test-organization")
     end
   end
+
+  describe 'organization account store mapping' do
+    def create_organization_account_store_mapping(organization, account_store)
+      test_api_client.organization_account_store_mappings.create({
+        account_store: { href: account_store.href },
+        organization: { href: organization.href }
+      })
+    end
+
+    context 'given an account_store is a directory' do
+      let(:directory) { test_api_client.directories.create name: random_directory_name }    
+
+      let(:organization_account_store_mapping) do
+        create_organization_account_store_mapping(organization, directory)
+      end
+  
+      let(:reloaded_mapping) do
+        test_api_client.account_store_mappings.get organization_account_store_mapping.href
+      end
+
+      after do
+        organization.delete if organization
+        directory.delete if directory
+      end
+
+      it 'should return a directory' do
+        expect(reloaded_mapping.account_store.class).to eq(Stormpath::Resource::Directory)
+        expect(reloaded_mapping.account_store).to eq(directory)
+      end
+    end
+
+    context 'given an account_store is a group' do
+      let(:directory) { test_api_client.directories.create name: random_directory_name }    
+    
+      let(:group) { directory.groups.create name: "test_group" }    
+
+      let(:organization_account_store_mapping) do
+        create_organization_account_store_mapping(organization, group)
+      end
+
+      let(:reloaded_mapping) do
+        test_api_client.account_store_mappings.get organization_account_store_mapping.href
+      end
+
+      after do
+        organization.delete if organization
+        group.delete if group
+        directory.delete if directory
+      end
+
+      it 'should return group' do
+        expect(reloaded_mapping.account_store.class).to eq(Stormpath::Resource::Group)
+        expect(reloaded_mapping.account_store).to eq(group)
+      end
+    end
+  end
 end
