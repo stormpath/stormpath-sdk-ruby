7ceabb63730b41306fa14049732239e0c3f6056d
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-11-18 16:06:48 +0100

    adding additional fields to organizations

--- lib/stormpath-sdk/resource/organization.rb
+++ lib/stormpath-sdk/resource/organization.rb
@@ -1,7 +1,8 @@
 class Stormpath::Resource::Organization < Stormpath::Resource::Instance
   include Stormpath::Resource::CustomDataStorage
 
-  prop_accessor :name, :description, :name_key, :status
+  prop_accessor :name, :description, :name_key, :status, :account_store_mappings,
+    :default_account_store_mapping, :default_group_store_mapping
 
   has_many :groups
   has_many :accounts
