9cac964fe34a737f92d3fd24d40407e3dca4f480
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-11-18 15:56:06 +0100

    adding additional fields to organization account store mappigns

--- lib/stormpath-sdk/resource/organization_account_store_mapping.rb
+++ lib/stormpath-sdk/resource/organization_account_store_mapping.rb
@@ -1,3 +1,6 @@
 class Stormpath::Resource::OrganizationAccountStoreMapping < Stormpath::Resource::Instance
-  
+  prop_accessor :is_default_account_store, :is_default_group_store, :list_index
+
+  belongs_to :organization
+  belongs_to :account_store
 end
