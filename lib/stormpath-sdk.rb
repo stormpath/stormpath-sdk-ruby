e3864152e6f9c4514b6732188b154329839beae1
Nenad Nikolic <nenad.nikolic@infinum.hr>
2015-12-17 14:00:25 +0100

    github providers autoload

--- lib/stormpath-sdk.rb
+++ lib/stormpath-sdk.rb
@@ -84,6 +84,8 @@ module Stormpath
     autoload :GoogleProviderData, 'stormpath-sdk/provider/google/google_provider_data'
     autoload :LinkedinProvider, 'stormpath-sdk/provider/linkedin/linkedin_provider'
     autoload :LinkedinProviderData, 'stormpath-sdk/provider/linkedin/linkedin_provider_data'
+    autoload :GithubProvider, 'stormpath-sdk/provider/github/github_provider'
+    autoload :GithubProviderData, 'stormpath-sdk/provider/github/github_provider_data'
     autoload :StormpathProvider, 'stormpath-sdk/provider/stormpath/stormpath_provider'
     autoload :StormpathProviderData, 'stormpath-sdk/provider/stormpath/stormpath_provider_data'
   end
