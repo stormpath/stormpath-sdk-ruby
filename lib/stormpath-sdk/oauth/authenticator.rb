a5a9e92d64b817f25cfecec39af03fe5c4e46db4
Damir Svrtan <damir.svrtan@gmail.com>
2016-06-16 15:48:02 +0200

    Refactor: stormpath-sdk/oauth/authenticator.rb

--- lib/stormpath-sdk/oauth/authenticator.rb
+++ lib/stormpath-sdk/oauth/authenticator.rb
@@ -7,21 +7,25 @@ module Stormpath
         @data_store = data_store
       end
 
-      def authenticate parent_href, request
+      def authenticate(parent_href, request)
         assert_not_nil parent_href, "parent_href must be specified"
 
-        if request.grant_type == 'password'
-          attempt = @data_store.instantiate PasswordGrant
-        elsif request.grant_type == 'refresh_token'
-          attempt = @data_store.instantiate RefreshToken
-        elsif request.grant_type == 'id_site_token'
-          attempt = @data_store.instantiate IdSiteGrant
-        end
-
+        grant_class = classes_by_grant_type[request.grant_type.to_sym]
+        attempt = @data_store.instantiate(grant_class)
         attempt.set_options(request)
 
         href = parent_href + '/oauth/token'
         @data_store.create href, attempt, Stormpath::Oauth::AccessTokenAuthenticationResult
+      end
+
+      private
+
+      def classes_by_grant_type
+        {
+          password: PasswordGrant,
+          refresh_token: RefreshToken,
+          id_site_token: IdSiteGrant
+        }
       end
     end
   end
