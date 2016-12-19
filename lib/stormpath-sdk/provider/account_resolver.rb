14454f84c48bb89639eb191cafe27c08d28d15cb
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2016-12-19 13:06:45 +0100

    Add optional account store parameter in account request and account resolver for provider directories

--- lib/stormpath-sdk/provider/account_resolver.rb
+++ lib/stormpath-sdk/provider/account_resolver.rb
@@ -18,26 +18,28 @@ module Stormpath
     class AccountResolver
       include Stormpath::Util::Assert
 
-      def initialize data_store
+      def initialize(data_store)
         @data_store = data_store
       end
 
-      def resolve_provider_account parent_href, request
-        assert_not_nil parent_href, "parent_href argument must be specified"
-        assert_kind_of AccountRequest, request, "Only #{AccountRequest} instances are supported."
+      def resolve_provider_account(parent_href, request)
+        assert_not_nil(parent_href, 'parent_href argument must be specified')
+        assert_kind_of(AccountRequest, request, "Only #{AccountRequest} instances are supported.")
 
         attempt = @data_store.instantiate AccountAccess
 
-        attempt.provider_data = {
-                                  request.token_type.to_s.camelize(:lower) => request.token_value,
-                                  "providerId" => request.provider
-                                }
+        attempt.provider_data = {}.tap do |body|
+          body[request.token_type.to_s.camelize(:lower)] = request.token_value
+          body['providerId'] = request.provider
+          if request.account_store.present?
+            body['accountStore'] = { request.account_store.first.first.to_s.camelize(:lower) => request.account_store.first.last }
+          end
+        end
 
-        href = parent_href + '/accounts'
+        href = "#{parent_href}/accounts"
 
-        @data_store.create href, attempt, Stormpath::Provider::AccountResult
+        @data_store.create(href, attempt, Stormpath::Provider::AccountResult)
       end
-
     end
   end
 end
