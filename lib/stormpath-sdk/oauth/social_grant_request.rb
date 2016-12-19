1487af68402139af3e4238124bc35183a3b74958
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2016-12-19 14:48:55 +0100

    Add redirect_uri parameter to social grant and social grant request

--- lib/stormpath-sdk/oauth/social_grant_request.rb
+++ lib/stormpath-sdk/oauth/social_grant_request.rb
@@ -1,12 +1,13 @@
 module Stormpath
   module Oauth
     class SocialGrantRequest
-      attr_accessor :grant_type, :provider_id, :code, :access_token
+      attr_accessor :grant_type, :provider_id, :code, :access_token, :redirect_uri
 
       def initialize(provider_id, options = {})
         @provider_id = provider_id.to_s
         @code = options[:code]
         @access_token = options[:access_token]
+        @redirect_uri = options[:redirect_uri]
         @grant_type = 'stormpath_social'
       end
     end
