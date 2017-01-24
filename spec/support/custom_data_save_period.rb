90dee5ea5645e78dfbaacd29879ac6f9b18bcb1a
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2017-01-24 10:49:55 +0100

    Add description text for the CustomDataSavePeriod module

--- spec/support/custom_data_save_period.rb
+++ spec/support/custom_data_save_period.rb
@@ -1,3 +1,6 @@
+# The wait_for_custom_data_indexing method is used throughout the specs when we search
+# the recently saved custom data. Since Elasticsearch is being used in the backend,
+# a small time period is needed for the data to finish indexing.
 module Stormpath
   module Test
     module CustomDataSavePeriod
