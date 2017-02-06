3445470e53d799eeb07abc04a47aa806b489cc41
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2017-02-06 12:16:15 +0100

    Fix failed travis spec for saml service provider registration

--- spec/resource/saml_service_provider_registration_spec.rb
+++ spec/resource/saml_service_provider_registration_spec.rb
@@ -3,22 +3,25 @@ require 'spec_helper'
 describe Stormpath::Resource::SamlServiceProviderRegistration, vcr: true do
   let(:application) { test_api_client.applications.create(application_attrs) }
   let(:identity_provider) { application.saml_policy.identity_provider }
-  let(:service_provider_registration_attrs) do
-    {
-      status: 'ENABLED',
-      default_relay_state: 'example_jwt'
-    }
+  let(:assertion_consumer_service_url) { 'https://some.sp.com/saml/sso/post' }
+  let(:entity_id) { random_number }
+  let(:service_provider) do
+    Stormpath::Authentication::RegisterServiceProvider.new(
+      test_api_client, identity_provider, assertion_consumer_service_url, entity_id
+    ).call
   end
-  let(:service_provider_registration) do
-    identity_provider.saml_service_provider_registrations.create(service_provider_registration_attrs)
-  end
+  let(:service_provider_registration) { identity_provider.saml_service_provider_registrations.first }
 
   before do
-    stub_request(:post, "#{identity_provider.href}/samlServiceProviderRegistrations")
-      .to_return(body: Stormpath::Test.mocked_service_provider_registration)
+    service_provider
+    service_provider_registration.default_relay_state = 'example_jwt'
+    service_provider_registration.save
   end
 
-  after { application.delete }
+  after do
+    service_provider.delete
+    application.delete
+  end
 
   it 'instances should respond to attribute property methods' do
     expect(service_provider_registration).to be_a Stormpath::Resource::SamlServiceProviderRegistration
