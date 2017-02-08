2868bac3839cbf214a3a56d04eb0c9323dabf294
Marko Ćilimković <marko.cilimkovic@infinum.hr>
2017-02-08 10:07:17 +0100

    Refactor RegisterServiceProvider to use data_store.create

--- lib/stormpath-sdk.rb
+++ lib/stormpath-sdk.rb
@@ -99,6 +99,7 @@ module Stormpath
     autoload :HttpBearerAuthentication, 'stormpath-sdk/auth/http_bearer_authentication'
     autoload :CreateFactor, 'stormpath-sdk/auth/create_factor'
     autoload :RegisterServiceProvider, 'stormpath-sdk/auth/register_service_provider'
+    autoload :RegisteredServiceProviderAttempt, 'stormpath-sdk/auth/registered_service_provider_attempt'
   end
 
   module Provider
--- lib/stormpath-sdk/auth/register_service_provider.rb
+++ lib/stormpath-sdk/auth/register_service_provider.rb
@@ -28,31 +28,22 @@ module Stormpath
       end
 
       def service_provider
-        # We could use the default create method for resource creation but we need to figure
-        # out the best way to create a class that's a RegisteredSamlServiceProvider request
-        # (it should be the same as the RegisteredSamlServiceProvider class..can we use it?)
-        # @service_provider ||= data_store.create(
-        #   service_provider_registration_href,
-        #   service_provider_registration_params,
-        #   Stormpath::Resource::RegisteredSamlServiceProvider
-        # )
-        @service_provider ||= data_store.execute_raw_request(
+        @service_provider ||= data_store.create(
           service_provider_registration_href,
-          service_provider_registration_params,
+          registered_service_provider_attempt,
           Stormpath::Resource::RegisteredSamlServiceProvider
         )
       end
 
-      def service_provider_registration_params
-        # In case we use the default data_store.create, dont forget to change the parameter
-        # names to snake case style
-        {}.tap do |body|
-          body[:assertionConsumerServiceUrl] = assertion_consumer_service_url
-          body[:entityId] = entity_id
-          body[:name] = name
-          body[:description] = description
-          body[:nameIdFormat] = name_id_format
-        end.compact
+      def registered_service_provider_attempt
+        @registered_service_provider_attempt ||=
+          data_store.instantiate(RegisteredServiceProviderAttempt).set_attributes(
+            assertion_consumer_service_url: assertion_consumer_service_url,
+            entity_id: entity_id,
+            name: name,
+            description: description,
+            name_id_format: name_id_format
+          )
       end
 
       def service_provider_registration_href
